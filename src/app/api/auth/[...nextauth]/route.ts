746664c27e79f1ffb6856511a3e7c7b9921d6572
Phoenix-Dev <116443234+Phoenix-Dev1@users.noreply.github.com>
2025-02-19 23:40:56 +0200

    [DEPLOY] - #3 - Fix ESLint export AuthOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -61,10 +61,4 @@ export const authOptions: NextAuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export {
-  handler as GET,
-  handler as POST,
-  handler as UPDATE,
-  handler as PUT,
-  handler as DELETE,
-};
+export { handler as GET, handler as POST };
